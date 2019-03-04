1814f138d787931284e5b3736272c392e011dc3c
U-ZOOPC305\mw283 <ZOOPC305+mw283@ZooPC305.biosci.cam.ac.uk>
2019-03-04 17:29:51 +0000

    new file:   draw-circle/patch.cpp 	new file:   draw-circle/patch.xodp 	modified:   draw-line/patch.cpp 	modified:   draw-line/patch.xodp

--- draw-circle/patch.cpp
+++ draw-circle/patch.cpp
@@ -12,13 +12,16 @@ void evaluate(Context ctx) {
     // Get a pointer to the `Adafruit_SSD1306` class instance
     auto display = getValue<input_DEV>(ctx);
 
-    auto x0 = getValue<input_X0>(ctx);
-    auto y0 = getValue<input_Y0>(ctx);
-    auto x1 = getValue<input_X1>(ctx);
-    auto y1 = getValue<input_Y1>(ctx);
-    auto colour = getValue<input_COLOUR>(ctx);
+    auto x0 = getValue<input_X>(ctx);
+    auto y0 = getValue<input_Y>(ctx);
+    auto radius = getValue<input_RAD>(ctx);
+    auto fill = getValue<input_FILL>(ctx);
 
-    display->drawLine(x0, y0, x1, y1, colour);
+    if (fill){
+        
+    }
+
+    display->drawLine(x0, y0, x1, y1, WHITE);
 
     emitValue<output_DONE>(ctx, 1);
 }
