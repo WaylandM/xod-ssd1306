cf1970ff55fec9d6d5763a78ab4b00801cbd2fd9
Matt <mw283@cam.ac.uk>
2019-03-05 00:40:56 +0000

    modified:   draw-circle/patch.xodp 	modified:   draw-line/patch.xodp 	new file:   draw-pixel/patch.cpp 	new file:   draw-pixel/patch.xodp 	new file:   test-draw-pixel/patch.xodp 	new file:   test-line/patch.xodp

---
+++ draw-pixel/patch.cpp
@@ -0,0 +1,22 @@
+
+struct State {
+};
+
+{{ GENERATED_CODE }}
+
+void evaluate(Context ctx) {
+    // The node responds only if there is an input pulse
+    if (!isInputDirty<input_UPD>(ctx))
+        return;
+
+    // Get a pointer to the `Adafruit_SSD1306` class instance
+    auto display = getValue<input_DEV>(ctx);
+
+    auto x = getValue<input_X>(ctx);
+    auto y = getValue<input_Y>(ctx);
+    auto colour = getValue<input_COLOUR>(ctx);
+
+    display->drawPixel(x, y, colour);
+
+    emitValue<output_DONE>(ctx, 1);
+}
